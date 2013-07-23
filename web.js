b5526a3395cb4690c4e13ff1dcf25c2b2d2f1fe1
wisemoon <willyedoo@gmail.com>
2013-07-24 00:39:30 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,10 @@
-var express = require('express');
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+var express = require('express') , http = require('http') , fs = require('fs');
+     var app = express();
+        app.set('port', process.env.PORT || 3000);
+          app.get('/', function(req,res){ 
+                   fs.readFile(__dirname +'/public/index.html', 'utf8', function(err,text){ 
+                                 res.send(text); 
+                                      });
+http.createServer(app).listen(app.get('port'), function(){ 
+            console.log('Server started at ' + app.get('port')); 
+      });
